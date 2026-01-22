6db1fdecb04137aab8fe87ba558130ba5563cd32
Matias <escobarmatiash@gmail.com>
2026-01-22 13:03:48 -0300

    Modelado de Torneo e Inscripcion

--- src/modelo/Inscripcion.java
+++ src/modelo/Inscripcion.java
@@ -1,5 +1,26 @@
 package modelo;
 
+import enums.EstadoInscripcion;
+
 public class Inscripcion {
+	private Socio socio;
+	private EstadoInscripcion estado;
+
+	public Inscripcion(Socio socio) {
+		this.socio = socio;
+		this.estado = EstadoInscripcion.ACTIVA;
+	}
+
+	public Socio getSocio() {
+		return socio;
+	}
+
+	public EstadoInscripcion getEstado() {
+		return estado;
+	}
+
+	public void darDeBaja() {
+		this.estado = EstadoInscripcion.BAJA;
+	}
 
 }
--- src/modelo/Torneo.java
+++ src/modelo/Torneo.java
@@ -1,5 +1,43 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import enums.EstadoTorneo;
+
 public class Torneo {
+	private String nombre;
+	private EstadoTorneo estado;
+	private List<Inscripcion> inscripciones;
+
+	public Torneo(String nombre) {
+		this.nombre = nombre;
+		this.estado = EstadoTorneo.CREADO;
+		this.inscripciones = new ArrayList<>();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public EstadoTorneo getEstado() {
+		return estado;
+	}
+
+	public void iniciar() {
+		this.estado = EstadoTorneo.EN_CURSO;
+	}
+
+	public void finalizar() {
+		this.estado = EstadoTorneo.FINALIZADO;
+	}
+
+	public void agregarInscripcion(Inscripcion inscripcion) {
+		inscripciones.add(inscripcion);
+	}
+
+	public List<Inscripcion> getInscripciones() {
+		return inscripciones;
+	}
 
 }
